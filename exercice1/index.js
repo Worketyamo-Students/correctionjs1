e3e3fee9e159f4e1e3f4e75d8fda55d4da19100c
BITAERIKA <mariane.azambou@facsciences-uy1.cm>
2026-05-28 17:02:35 +0100

    first commit

---
+++ exercice1/index.js
@@ -0,0 +1,18 @@
+// let age = prompt(" entrez votre age");
+// console.log(typeof (age));
+// age = Number(age);
+// console.log(typeof (age));
+// if (age >= 18) {
+//     console.log("acces autoriser");
+//     alert(" biemvenue");
+// } else{
+//     console.error( " erreur");
+//     alert("acces refuser");
+// }
+
+
+let ht = Number( prompt('entrez le prix ht: '));
+let TTC = ht * 1.2;
+console.log("le prix TTC = " + TTC + "FCFA");
+console.log(`le prix TTC = ${TTC} FCFA`)
+
