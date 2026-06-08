bbc255d51527748703c5656e3bcd369a4d15f0a5
BITAERIKA <mariane.azambou@facsciences-uy1.cm>
2026-06-08 12:49:49 +0100

    feat: fiche2 exo1

--- exercice1/index.js
+++ exercice1/index.js
@@ -11,8 +11,18 @@
 // }
 
 
-let ht = Number( prompt('entrez le prix ht: '));
-let TTC = ht * 1.2;
-console.log("le prix TTC = " + TTC + "FCFA");
-console.log(`le prix TTC = ${TTC} FCFA`)
+// let ht = Number( prompt('entrez le prix ht: '));
+// let TTC = ht * 1.2;
+// console.log("le prix TTC = " + TTC + "FCFA");
+// console.log(`le prix TTC = ${TTC} FCFA`)
 
+
+//fiche2
+
+//exo1
+
+const nom = 'iphone_15';
+let prixNum = parseInt(prompt("Entrez le prix: "));
+console.log(prixNum);
+let estEnStocke = true;
+let a = null;
